2ee02d05f780768572d9deb9a6a4abf9a81a37ac
Alex Rodrigues <alex2199rodrigues@gmail.com>
2025-02-22 20:35:51 -0300

    Update ex021.py

--- Mundo1/ex021.py
+++ Mundo1/ex021.py
@@ -5,7 +5,7 @@ from playsound import playsound
 
 
 # Passo 1: Definir um valor para "arquivo MP3"
-audio = 'Mundo1\ex021_Icecreamtruckmelodysong.wav' # arquivo de áudio
+audio = 'C:/Users/Usuario/Documents/MeusProjetos/curso-python3/Mundo1/ex021_Icecreamtruckmelodysong.wav' # arquivo de áudio
 
 # Passo 2: Reproduzir o som
 playsound(audio) # reprodução
