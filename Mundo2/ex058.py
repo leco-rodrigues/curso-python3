40ccce4253aad525364eb1307c0e668696dc33b6
Alex Rodrigues <alex2199rodrigues@gmail.com>
2025-03-13 10:48:28 -0300

    Update ex058.py

--- Mundo2/ex058.py
+++ Mundo2/ex058.py
@@ -1,34 +1,41 @@
 # Melhore o jogo do DESAFIO 028 onde o computador vai "pensar" em um número entre 0 e 10.
 # Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.
 
-from modulo import txt_style as styl, sep
+from custom_module import txt_style as styl, sepa_rator as sep_
 from random import randint
 from time import sleep
 
 
 # Passo 1: Solicitar valores para "número" e "palpite"
-print(styl(sep(7), color_ = 'c'), end = " ")
-print(styl("ADIVINHE O NÚMERO", 'bd', 'p'), end = " ")
-print(styl(sep(7), color_ = 'c'))
+print(styl(f"\t{' GUESS THE NUMBER ':=^40}\n", 'bd'))
 
-print(styl("\nAguarde enquanto o computador está processando um número aleatório entre 0 e 10...\n", 'bd', 'p'))
+print(styl("Please wait while the computer is processing a number between 0 and 10...\n", 'bd'))
+
 sleep(1.5)
 
 num: int = randint(0, 10)
 cont: int = 0
 
 while True:
-    palpite: int = int(input(styl("Qual foi o número sorteado?", 'n') + " "))
+    print(sep_(color = 'c'))
+    palpite: int = int(input(styl("Guess the number:", 'n') + " "))
     cont += 1
+    print(sep_(color = 'c'))
 
     if palpite == num:
+        print(styl(f'You guessed it! Congratulations! The drawn number was {num}.', 'bd', 'g'))
         break
 
-    print(styl("\nVocê errou! Por favor, tente novamente.", 'bd', 'r'))
-    print(styl(sep(16), color_ = 'c'))
+    elif palpite < num:
+        print(styl("The number is higher... Please try again.", 'bd', 'y'))
+
+    elif palpite > num:
+        print(styl("The number is lower... Please try again.", 'bd', 'b'))
 
 # Passo 2: Exibir número de tentativas
-print(styl(sep(16), color_ = 'c'))
-print(styl(f'Parabéns! Você acertou! O número correto era {num}.', 'bd', 'p'))
-print(styl(f'Número de tentativas: {cont}', 'bd', 'b'))
-# ---------------------------------------------------------| Desafio [058]
+print(sep_(color = 'c'))
+print(styl(f'Number of attempts: {cont}', 'bd', 'p'))
+
+print(sep_(color = 'c'))
+print(styl("END", 'bd'))
+# ---------------------| Desafio [058]
