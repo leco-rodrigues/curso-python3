d004dab94671e59175de50ea9d1a5783a069c778
Alex Rodrigues <alex2199rodrigues@gmail.com>
2025-03-14 17:12:25 -0300

    Update ex063.py

--- Mundo2/ex063.py
+++ Mundo2/ex063.py
@@ -1,27 +1,38 @@
-# Escreva um programa que leia um número inteiro qualquer e mostre na tela os n primeiros elementos de uma Sequência de Fibonacci.
-    # Ex:
-    # 0 → 1 → 1 → 2 → 3 → 5 → 8
+# Sequência de Fibonacci v1.0:
+    # Escreva um programa que leia um número n inteiro qualquer e mostre na tela os n primeiros elementos de uma Sequência de Fibonacci.
+        # Ex:
+        # 0 → 1 → 1 → 2 → 3 → 5 → 8
 
-from Mundo2.custom_module import txt_style as styl, sep
+from custom_module import txt_style as styl, print_error, sepa_rator as sep_
 
 
 # Passo 1: Solicitar um valor para "número inteiro"
-print(styl(f"{' FIBONACCI ':=^40}", 'bd', 'p') + "\n")
-n: int = int(input(styl("How many elements would you like to see?", 'n') + " "))
-print(styl(sep(15), color_ = 'c'))
+print(styl(f"\t{' FIBONACCI ':=^40}", 'bd') + "\n")
+
+while True:
+    try:
+        num: int = int(input(styl("How many elements would you like to see?", 'n') + " "))
+        break
+    except ValueError:
+        print_error(16)
+
+print(sep_(16, 'c'))
 
 # Passo 2: Exibir os "n" primeiros elementos de uma Sequência Fibonacci
 print(styl("0 → 1 → ", 'bd', 'b'), end = "")
 
-fib0 = 0
-fib1 = 1
-cont = 0
-while cont < n - 2:
-    fibn = fib0 + fib1
-    print(styl(f"{fibn}", 'bd', 'b'), end = " → ")
-    fib0 = fib1
-    fib1 = fibn
+term_1 = 0
+term_2 = 1
+cont = 1
+while cont <= num - 2:
+    current_term = term_1 + term_2
+    print(styl(f"{current_term}", 'bd', 'b'), end = " → ")
+    term_1 = term_2
+    term_2 = current_term
     cont += 1
 
 print(styl("END", 'bd', 'y'))
-# --------------------------| AULA 14 - ESTRUTURA DE REPETIÇÃO "WHILE" | DESAFIO [063]
+
+print(sep_(16, 'c'))
+print(styl("Exiting program...", 'bd'))
+# ------------------------------------| AULA 14 - ESTRUTURA DE REPETIÇÃO "WHILE" | DESAFIO [063]
