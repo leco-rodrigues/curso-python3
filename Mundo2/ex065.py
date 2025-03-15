1d990f0c0a47a2685010f3abeef671e2823ea370
Alex Rodrigues <alex2199rodrigues@gmail.com>
2025-03-14 23:09:26 -0300

    Update ex065.py

--- Mundo2/ex065.py
+++ Mundo2/ex065.py
@@ -1,36 +1,54 @@
-# Crie um programa que leia vários números inteiros pelo teclado.
-# No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos.
-# O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+# Maior e Menor valores:
+    # Crie um programa que leia vários números inteiros pelo teclado.
+    # No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos.
+    # O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
 
-from Mundo2.custom_module import txt_style as styl, sep
+from custom_module import txt_style as styl, yes_no, print_error, sepa_rator as sep_
 
 
 # Passo 1: Solicitar valores para "número inteiro"
-print(styl(f"{' MEAN ':=^40}", 'bd', 'p') + "\n")
+print(styl(f"\t{' MEAN, BIGGEST & LOWER ':=^40}", 'bd') + "\n")
 
 numbers: list[int] = []
 while True:
-    num: int = int(input(styl("Enter a number:", 'n') + " "))
-    numbers.append(num)
+    try:
+        num: int = int(input(styl("Please enter a number:", 'n') + " "))
+        numbers.append(num)
 
-    add_num: str = str(input(styl("Would you like to enter one more number (s/n)?", 'n') + " "))
-    if add_num == 'n':
-        break
+        while True:
+            add_num: str = str(input(styl("Would you like to enter one more number (y/n)?", 'n') + " ")).strip().lower()
+                
+            if add_num in ('yes', 'y', 'no', 'n'):
+                break
+            else:
+                print_error()
 
-print(styl(sep(15), color_ = 'c'))
+        if yes_no(add_num):
+            continue
+        else:
+            break
+
+    except ValueError:
+        print_error()
+
+print(sep_(15, 'c'))
 
 # Passo 2: Exibir a média
-mean = sum(numbers) / len(numbers)
-print(styl(f"The mean between these numbers is {mean:,.2f}.", 'bd', 'b'))
-print(styl(sep(15), color_ = 'c'))
+if len(numbers) > 1:
+    print(styl(f"The mean between these numbers is {sum(numbers) / len(numbers):,.2f}.", 'bd', 'b'))
+    print(sep_(15, 'c'))
 
 # Passo 3: Exibir o maior
-print(styl(f"The biggest number is {max(numbers)}.", 'bd', 'b'))
-print(styl(sep(15), color_ = 'c'))
+    print(styl(f"The biggest number is {max(numbers)}.", 'bd', 'b'))
+    print(sep_(15, 'c'))
 
 # Passo 4: Exibir o menor
-print(styl(f"The lowest number is {min(numbers)}.", 'bd', 'b'))
-print(styl(sep(15), color_ = 'c'))
+    print(styl(f"The lowest number is {min(numbers)}.", 'bd', 'b'))
+    print(sep_(15, 'c'))
 
-print(styl("END", 'bd', 'y'))
-# --------------------------| AULA 14 - ESTRUTURA DE REPETIÇÃO "WHILE" | DESAFIO [065]
+else:
+    print(styl(f"The only number is {numbers[0]}.", 'bd', 'p'))
+    print(sep_(15, 'c'))
+
+print(styl("Exiting program...", 'bd'))
+# ------------------------------------| AULA 14 - ESTRUTURA DE REPETIÇÃO "WHILE" | DESAFIO [065]
