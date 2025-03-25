bc4f071e12fdc98dde518e39632d05c4f7b93b88
Alex Rodrigues <alex2199rodrigues@gmail.com>
2025-03-25 16:18:09 -0300

    Update ex073.py

--- Mundo3/ex073.py
+++ Mundo3/ex073.py
@@ -8,25 +8,28 @@
 
 # Passo 1: Criar uma tupla com os 20 primeiros colocados
 team_rankings: tuple[str, ...] = (
-    "Palmeiras", "Atlético Mineiro", "Flamengo", "Fluminense", "Internacional", "Atlético Paranaense",
-    "Corinthians", "São Paulo", "Grêmio", "Vasco da Gama", "Botafogo", "Ceará", "Fortaleza",
-    "Red Bull Bragantino", "Santos", "Coritiba", "Goiás", "Cuiabá", "América Mineiro", "Bahia"
+    "Palmeiras", "Atlético Mineiro", "Flamengo", "Fluminense", "Internacional",
+    "Atlético Paranaense", "Corinthians", "São Paulo", "Grêmio",
+    "Vasco da Gama", "Botafogo", "Ceará", "Fortaleza", "Red Bull Bragantino",
+    "Santos", "Coritiba", "Goiás", "Cuiabá", "América Mineiro", "Bahia"
 )
 
+print(f"\tTeam Rankings:\n {team_rankings}\n")
+
 # Passo 2: Exibir os 5 primeiros colocados
-print("Os 5 primeiros colocados na tabela são:")
+print("The top five teams are:")
 print(team_rankings[0:5])
 
 # Passo 3: Exibir os 4 últimos colocados
-print("\nOs 4 últimos colocados são:")
+print("\nThe bottom four teams are:")
 print(team_rankings[-4:])
 
 # Passo 4: Exibir em ordem alfabética
-print("\nA tabela em ordem alfabética fica:")
+print("\nThe table in alphabetical order is:")
 print(sorted(team_rankings))
 
 # Passo 5: Exibir a posição da "Vasco da Gama"
-print(f"\nO Vasco da Gama está na {team_rankings.index('Vasco da Gama') + 1}ª posição.")
+print(f"\nVasco da Gama is in {team_rankings.index('Vasco da Gama') + 1}ª position.")
 
 # Passo 6: Exibir mensagem de encerramento
 print("\nExiting program... Thank you for using it! 😄")
