76947c3218eceae919abd960e8b1128dc87bf801
Alex Rodrigues <alex2199rodrigues@gmail.com>
2025-04-13 22:12:59 -0300

    Update ex092.py

--- Mundo3/ex092.py
+++ Mundo3/ex092.py
@@ -2,20 +2,44 @@
     # Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-os (com idade) em um dicionário se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário.
     # Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar (35 anos de colaboração).
 
+from sys import path
+path.append("C:/Users/Usuario/Documents/MeusProjetos/curso-python3")
+from functions import txt_style as styl, print_error, sepa_rator as sep_
 from datetime import datetime
 
 
 # Passo 1: Solicitar valores para "nome", "nascimento" e "CTPS" (com "contratação" e "salário")
-print("----------\nSTAFF REGISTRY\n----------\n")
+sep_(10, "c")
+print(styl("STAFF REGISTRY", "bd"))
+sep_(10, "c")
 
 worker_data: dict[str, object] = {}
 
-name: str = "Alex"
-birth_year: int = 1999
-ctps: int = 2019
+while True:
+    name: str = str(input(styl("Name: ", "n"))).strip()
+    if (not name) or (not all(c.isalpha() or c.isspace() for c in name)):
+        print_error("Invalid input! Please enter a valid name.")
+        continue
+    break
+
+current_year = datetime.now().year
+while True:
+    try:
+        birth_year: int = int(input(styl("Year of birth: ", "n")))
+        if 1920 > birth_year or current_year <= birth_year:
+            print_error(f"Invalid input! Please enter a valid year between 1920 and {current_year}.")
+            continue
+        break
+    except ValueError:
+        print_error("Invalid input! Please enter a valid year.")
+
+while True:
+    try:
+        ctps: int = int(input(styl("CTPS: (0 to none) ", "n")))
+    except ValueError:
+        print_error("Invalid input! Please enter a valid year.")
 
 # Passo 2: Acrescentar "idade" e "ano de aposentadoria"
-current_year = datetime.now().year
 age: int = current_year - birth_year
 
 worker_data["Name"] = name
