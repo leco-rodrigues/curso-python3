fa2c34055129dd1832281ebc5e493149145e71de
Leco <alex2199rodrigues@gmail.com>
2025-04-14 15:48:51 -0300

    Create ex094.py

---
+++ Mundo3/ex094.py
@@ -0,0 +1,14 @@
+# Unindo dicionários e listas:
+    # Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um dicionário e todos os dicionários em uma lista.
+    # No final, mostre:
+        # A) Quantas pessoas foram cadastradas.
+        # B) A média de idade do grupo.
+        # C) Uma lista com todas as mulheres.
+        # D) Uma lista com todas as pessoas com idade acima da média.
+
+# Passo 1: 
+
+
+# Passo : Exibir mensagem de encerramento
+print("\n----------\nProgram finished. Thanks for using it! 😄\n----------\n")
+# ----------------------------------------------------------------------------| AULA 19 - DICIONÁRIOS | DESAFIO [094]
