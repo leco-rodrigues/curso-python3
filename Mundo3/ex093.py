9bd740eac6e5829557402470b3d2d28e442a2ff9
Leco <alex2199rodrigues@gmail.com>
2025-04-14 15:48:48 -0300

    Create ex093.py

---
+++ Mundo3/ex093.py
@@ -0,0 +1,12 @@
+# Cadastro de Jogador de Futebol:
+    # Crie um programa que gerencie o aproveitamento de um jogador de futebol.
+    # O programa vai ler o nome do jogador e quantas partidas ele jogou.
+    # Depois vai ler a quantidade de gols feitos em cada partida.
+    # No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
+
+# Passo 1: 
+
+
+# Passo : Exibir mensagem de encerramento
+print("\n----------\nProgram finished. Thanks for using it! 😄\n----------\n")
+# ----------------------------------------------------------------------------| AULA 19 - DICIONÁRIOS | DESAFIO [093]
