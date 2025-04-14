8df6063b99e26642504d90e37954087609d369fd
Leco <alex2199rodrigues@gmail.com>
2025-04-14 15:48:54 -0300

    Create ex095.py

---
+++ Mundo3/ex095.py
@@ -0,0 +1,9 @@
+# Aprimorando os dicionários:
+    # Aprimore o DESAFIO 093 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.
+
+# Passo 1: 
+
+
+# Passo : Exibir mensagem de encerramento
+print("\n----------\nProgram finished. Thanks for using it! 😄\n----------\n")
+# ----------------------------------------------------------------------------| AULA 19 - DICIONÁRIOS | DESAFIO [095]
