4a634a74e0a290b6c005361355d091adffad8edd
Leco <alex2199rodrigues@gmail.com>
2025-05-07 09:42:17 -0300

    Update ex099.py

--- Mundo3/ex099.py
+++ Mundo3/ex099.py
@@ -2,19 +2,22 @@
     # Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros.
     # Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
 
-def show_exit_message(message: str = None) -> None:
+from time import sleep
+
+
+def display_exit_message(message: str = None) -> None:
     if message is None:
         message = "Exiting program... Thanks for using it! 😄"
-    show_formated_message(message, "-")
+    display_message_box(message, "-")
 
 
-def show_error_message(message: str = None) -> None:
+def display_error(message: str = None) -> None:
     if (not message):
         message = "Invalid input! Please check."
-    show_formated_message(f"Invalid input: {message}", "-", 7, False)
+    display_message_box(f"Invalid input: {message}", "-", 7, False)
 
 
-def show_formated_message(text: str = None, sep: str = None, sep_multi: int = None, central: bool = True) -> None:
+def display_message_box(text: str = None, sep: str = None, sep_multi: int = None, central: bool = True) -> None:
     if (not text):
         text = "Olá, Mundo!"
     if (not sep):
@@ -28,7 +31,7 @@ def show_formated_message(text: str = None, sep: str = None, sep_multi: int = No
     print(message)
 
 
-def yes_no(question: str = None) -> bool:
+def prompt_yes_no(question: str = None) -> bool:
     if (not question):
         question = "Do you want to continue? (y/n)"
 
@@ -43,7 +46,7 @@ def yes_no(question: str = None) -> bool:
 
             break
         except ValueError as e:
-            show_error_message(f"{e}")
+            display_error(f"{e}")
 
     if (choice in ("no", "n")):
         return False
@@ -51,7 +54,7 @@ def yes_no(question: str = None) -> bool:
 
 
 # Passo 1: Criar função para mostrar o "maior valor" dentre todos os argumentos recebidos
-def number_input(pergunta: str) -> tuple:
+def collect_integers(pergunta: str) -> tuple:
     prompt: str = "\n-------\n" + pergunta.strip() + " "
     numbers_list: list[int] = []
 
@@ -72,22 +75,43 @@ def number_input(pergunta: str) -> tuple:
 
             numbers_list.append(int(numero))
         except ValueError as e:
-            show_error_message(f"{e}")
+            display_error(f"{e}")
             continue
 
-        if (not yes_no("Do you want to add another number to the list?")):
+        if (not prompt_yes_no("Do you want to add another number to the list?")):
             return tuple(numbers_list)
 
 
 def maior(*numeros: int) -> int:
-    print(max(numeros))
+    num_maior: int | None = None
+    count: int = 0
+
+    if (not numeros):
+        if (not prompt_yes_no("Deseja adicionar números para análise? (y/n)")):
+            display_message_box("Não há números para serem analisados", "-", 7, False)
+            return
+        numeros = collect_integers("Digite um número para análise:")
+
+    display_message_box("Analisando os números...", "-", 7, False)
+
+    for n in numeros:
+        print(n, end = " | ", flush = True)
+        if (num_maior is None) or (n > num_maior):
+            num_maior = n
+        count += 1
+        sleep(1)
+
+    print("FIM")
+    display_message_box(f"Quantidade de valores analisados: {count} \nMaior valor apresentado: {num_maior}", "-", 7, False)
 
 
 # Passo 2: Exibir o "maior valor"
-show_formated_message("EXIBINDO O MAIOR VALOR", "-")
+display_message_box("EXIBIR O MAIOR VALOR", "-")
 
-maior(number_input("Enter a number:"))
+maior(1, 2, 5, 8, 9, 7, 6, 3, 4, 1)
+maior(-1, -2, -8, -5, -3, -1)
+maior()
 
 # Passo 3: Exibir mensagem de encerramento:
-show_exit_message()
+display_exit_message()
 # ----------------| AULA 21 - FUNÇÕES (PARTE 1) | DESAFIO [099]
