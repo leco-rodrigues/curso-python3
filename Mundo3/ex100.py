1412cb9b1961e776570a87a709326e53098f4f59
Leco <alex2199rodrigues@gmail.com>
2025-05-07 19:03:17 -0300

    Update ex100.py

--- Mundo3/ex100.py
+++ Mundo3/ex100.py
@@ -2,13 +2,17 @@
     # Faça um programa que tenha uma lista chamada números e duas funções chamadas sorteia() e somaPar().
     # A primeira função vai sortear 5 números e vai colocá-los dentro da lista e a segunda função vai mostrar a soma entre todos os valores PARES sorteados pela função anterior.
 
-def show_exit_message(message: str = None) -> None:
+from random import randint
+from time import sleep
+
+
+def display_exit_message(message: str = None) -> None:
     if message is None:
         message = "Exiting program... Thanks for using it! 😄"
-    escreva(message, "-")
+    display_message_box(message, "-")
 
 
-def escreva(text: str = None, sep: str = None, sep_multi: int = None, central: bool = True) -> None:
+def display_message_box(text: str = None, sep: str = None, sep_multi: int = None, central: bool = True) -> None:
     if (not text):
         text = "Olá, Mundo!"
     if (not sep):
@@ -22,9 +26,58 @@ def escreva(text: str = None, sep: str = None, sep_multi: int = None, central: b
     print(message)
 
 
-# Passo 1: 
+# Passo 1: Criar uma função que sorteia 5 números e os coloca dentro de uma lista
+def sorteia() -> list:
+    numbers_list: list[int] = [randint(1, 100) for _ in range(5)]
+    return numbers_list
 
 
-# Passo : Exibir mensagem de encerramento:
-show_exit_message()
-# ----------------| AULA 21 - FUNÇÕES (PARTE 1) | DESAFIO [100]
+def display_list(numbers: list[int]) -> None:
+    display_message_box("EXIBINDO A LISTA DE NÚMEROS", "-")
+
+    for n in numbers:
+        print(n, end = " | ", flush = True)
+        sleep(1)
+    print("FIM")
+    sleep(1)
+
+
+# Passo 2: Criar função que soma os pares dentre os números sorteados
+def somaPar(random_int_list: list[int], pausar: bool = True) -> None:
+    display_message_box("EXIBINDO OS PARES", "-")
+
+    sumEven: int = 0
+    count: int = 0
+
+    for n in random_int_list:
+        if (n % 2 == 0):
+            print(n, end = " | ", flush = True)
+            sumEven += n
+            count += 1
+            if pausar:
+                sleep(1)
+    print("FIM\n")
+    sleep(1)
+
+    if (count == 0):
+        display_message_box("Não há números pares para serem somados.", "-", 7, False)
+        return
+
+    if (count == 1):
+        display_message_box(f"Houve apenas um número par: {sumEven}", "-", 7, False)
+        return
+
+    display_message_box(f"Soma dos pares: {sumEven}", "-", 7, False)
+
+
+# Passo 3: Exibir a lista sorteada
+random_numbers: list[int] = sorteia()
+display_list(random_numbers)
+
+# Passo 4: Exibir a soma dos pares
+somaPar(random_numbers)
+sleep(1)
+
+# Passo 5: Exibir mensagem de encerramento:
+display_exit_message()
+# -------------------| AULA 21 - FUNÇÕES (PARTE 1) | DESAFIO [100]
