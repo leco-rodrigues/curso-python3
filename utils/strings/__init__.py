5a092a5bba62e9cd7478c1e3fbcd4e5f7b2fef38
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-14 16:24:55 -0300

    Add function display_message_box() to module strings

--- utils/strings/__init__.py
+++ utils/strings/__init__.py
@@ -2,3 +2,16 @@ def exit_message(message: str = None) -> str:
     if(not message):
         message = "Exiting program... Thanks for using it! 😄"
     return message
+
+def display_message_box(text: str = None, sep: str = None, sep_multi: int = None, central: bool = True) -> None:
+    if(not text):
+        text = "Olá, Mundo!"
+    if(not sep):
+        sep = "~"
+    if(not sep_multi):
+        sep_multi = len(text) + 4
+    if(central):
+        text = "  " + text
+    separator: str = sep * sep_multi
+    message: str = f"\n{separator}\n{text}\n{separator}\n"
+    print(message)
