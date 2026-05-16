a769a15352d99f20019705f1ecd12ce73defe4e9
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-16 13:44:02 -0300

    Update diminuir() to support formatted output

--- utils/numbers/__init__.py
+++ utils/numbers/__init__.py
@@ -16,8 +16,11 @@ def aumentar(n: float, p: float, format: bool = True) -> float:
         n = moeda(n)
     return n
 
-def diminuir(n: float, p: float) -> float:
-    return n * (1 - (p / 100))
+def diminuir(n: float, p: float, format: bool = True) -> float:
+    n = n * (1 - (p / 100))
+    if(format):
+        n = moeda(n)
+    return n
 
 def moeda(n: float) -> str:
     return f"R${n:.2f}".replace(".", ",")
