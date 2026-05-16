5630067f5a46f4ba0e7db0b0ef9b14f004ba163f
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-16 14:33:27 -0300

    Add resumo() function to numbers module

--- utils/numbers/__init__.py
+++ utils/numbers/__init__.py
@@ -24,3 +24,17 @@ def diminuir(n: float, p: float, format: bool = True) -> float:
 
 def moeda(n: float) -> str:
     return f"R${n:.2f}".replace(".", ",")
+
+def resumo(n: float, a: float, d: float) -> None:
+    n_dobro: float = dobro(n)
+    n_metade: float = metade(n)
+    n_aumento: float = aumentar(n, a)
+    n_desconto: float = diminuir(n, d)
+    
+    print("-" * 25 + "\n     RESUMO DO VALOR\n" + "-" * 25)
+    print(f"Valor:           {moeda(n)}")
+    print(f"Dobro:           {n_dobro}")
+    print(f"Metade:          {n_metade}")
+    print(f"Aumento de {a}%:  {n_aumento}")
+    print(f"Desconto de {d}%: {n_desconto}")
+    print("-" * 25)
