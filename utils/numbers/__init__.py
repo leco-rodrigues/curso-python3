f4a95c491807d62df7f65d374f036751c5f9057b
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-16 13:41:02 -0300

    Update aumentar() to support formatted output

--- utils/numbers/__init__.py
+++ utils/numbers/__init__.py
@@ -10,13 +10,14 @@ def metade(n: float, format: bool = True) -> float:
         n = moeda(n)
     return n
 
-def aumentar(n: float, p: float) -> float:
-    return n * (1 + (p / 100))
+def aumentar(n: float, p: float, format: bool = True) -> float:
+    n = n * (1 + (p / 100))
+    if(format):
+        n = moeda(n)
+    return n
 
 def diminuir(n: float, p: float) -> float:
     return n * (1 - (p / 100))
 
 def moeda(n: float) -> str:
     return f"R${n:.2f}".replace(".", ",")
-
-print(dobro(10, False))
