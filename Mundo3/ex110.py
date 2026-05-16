a1f0a8b213d2a651ba55a3619356ec06b833a6a9
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-16 14:38:20 -0300

    Add challenge 110 solution

--- Mundo3/ex110.py
+++ Mundo3/ex110.py
@@ -1,12 +1,15 @@
 # Reduzindo ainda mias seu programa:
     # Adicione ao módulo moeda.py criado nos desafios anteriores,
-    # uma função chamda resumo(), que mostre na tela algumas informações
+    # uma função chamada resumo(), que mostre na tela algumas informações
     # geradas pelas funções que já temos no módulo criado até aqui.
 
+from utils.strings import exit_message, display_message_box
 # Passo 1: Adicionar a função
+from utils.numbers import resumo
 
 # Passo 2: Exibir o resultado
+resumo(100, 10, 50)
 
 # Passo 3: Exibir uma mensagem de encerramento
-
-# -------------------| AULA 22 - MÓDULOS E PACOTES | DESAFIO [110]
+display_message_box(exit_message())
+# --------------------------------| AULA 22 - MÓDULOS E PACOTES | DESAFIO [110]
