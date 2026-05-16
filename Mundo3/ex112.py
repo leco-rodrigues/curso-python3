89fd3272c80ffcb07fec2a3a689c94b162e4e979
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-16 15:57:33 -0300

    Add challenge 112 solution

--- Mundo3/ex112.py
+++ Mundo3/ex112.py
@@ -1,14 +1,19 @@
 # Entrada de dados monetários
-    # Dentro do pacote utilidades CeV que criamos no desafio 111,
+    # Dentro do pacote utilidadesCeV que criamos no desafio 111,
     # temos um módulo chamado dado.
     # Crie uma função chamada leiaDinheiro que seja capaz de funcionar
     # como a função input(), mas com uma validação de dados para aceitar
     # apenas valores que sejam monetários.
 
-# Passo 1: Criar uma função
+from utils.strings import exit_message, display_message_box
+# Passo 1: Criar a função
+from utils.numbers import resumo, leiaDinheiro
+
+valor: float = leiaDinheiro()
 
 # Passo 2: Exibir o resultado
+resumo(valor, 10, 50)
 
 # Passo 3: Exibir uma mensagem de encerramento
-
-# -------------------| AULA 22 - MÓDULOS E PACOTES | DESAFIO [112]
+display_message_box(exit_message())
+# --------------------------------| AULA 22 - MÓDULOS E PACOTES | DESAFIO [112]
