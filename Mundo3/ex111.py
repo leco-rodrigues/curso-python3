07fd18ef49ab9fef70a1e0855cf82e7b88513efb
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-16 15:12:22 -0300

    Add challenge 111 solution

--- Mundo3/ex111.py
+++ Mundo3/ex111.py
@@ -1,15 +1,25 @@
 # Transformando módulos em pacotes
     # Crie um pacote chamado utilidadesCeV que tenha dois módulos internos
     # chamados moeda e dado.
-    # Transfira todas as funções utilizadas nos desafios 107,108 e 109
+    # Transfira todas as funções utilizadas nos desafios 107, 108 e 109
     # para o primeiro pacote e mantenha tudo funcionando.
 
-# Passo 1: Criar um pacote
+from utils.strings import exit_message, display_message_box
+# Passo 1: Criar um pacote e transferir todas as funções
+from utils import numbers
 
-# Passo 2: Transferir todas as funções
+# Passo 2: Exibir o resultado
+valor: float = float(input("Digite um valor: R$"))
+aumento: float = float(input("Digite a porcentagem de aumento: "))
+desconto: float = float(input("Digite a porcentagem de desconto: "))
 
-# Passo 3: Exibir o resultado
+print(f"O dobro de {numbers.moeda(valor)} é igual a {numbers.dobro(valor, True)}.")
+print(f"A metade de {numbers.moeda(valor)} é igual a {numbers.metade(valor, True)}.")
+print(f"Um aumento de {aumento}% sobre {numbers.moeda(valor)} é igual a {numbers.aumentar(valor, aumento, True)}.")
+print(f"Um desconto de {desconto}% sobre {numbers.moeda(valor)} é igual a {numbers.diminuir(valor, desconto, True)}.")
 
-# Passo 4: Exibir uma mensagem de encerramento
+numbers.resumo(valor, aumento, desconto)
 
-# # -------------------| AULA 22 - MÓDULOS E PACOTES | DESAFIO [111]
+# Passo 3: Exibir uma mensagem de encerramento
+display_message_box(exit_message())
+# # ------------------------------| AULA 22 - MÓDULOS E PACOTES | DESAFIO [111]
