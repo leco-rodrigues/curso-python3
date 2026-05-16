f9f72ee37ec0c11bdf0072d3f83f129aae56e3aa
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-16 12:51:22 -0300

    Add challenge 108 solution

--- Mundo3/ex108.py
+++ Mundo3/ex108.py
@@ -3,10 +3,25 @@
     # criando uma função adicional chamada moeda() que consiga mostrar
     # os valores como um valor monetário formatado.
 
+from utils.strings import exit_message, display_message_box
 # Passo 1: Criar função adicional
+from utils import numbers
 
 # Passo 2: Exibir o resultado
+valor: float = float(input("Digite um valor: R$"))
+aumento: float = float(input("Deseja um aumento de quantos %? "))
+desconto: float = float(input("Deseja um desconto de quantos %? "))
+
+valor_dobro: float = numbers.dobro(valor)
+valor_metade: float = numbers.metade(valor)
+valor_aumentado: float = numbers.aumentar(valor, desconto)
+valor_descontado: float = numbers.diminuir(valor, desconto)
+
+print(f"O dobro de {numbers.moeda(valor)} é igual a {numbers.moeda(valor_dobro)}.")
+print(f"A metade de {numbers.moeda(valor)} é igual a {numbers.moeda(valor_metade)}.")
+print(f"Um aumento de {aumento}% sobre {numbers.moeda(valor)} é igual a {numbers.moeda(valor_aumentado)}.")
+print(f"Um desconto de {desconto}% sobre {numbers.moeda(valor)} é igual a {numbers.moeda(valor_descontado)}.")
 
 # Passo 3: Exibir uma mensagem de encerramento
-
-# # -------------------| AULA 22 - MÓDULOS E PACOTES | DESAFIO [108]
+display_message_box(exit_message())
+# --------------------------------| AULA 22 - MÓDULOS E PACOTES | DESAFIO [108]
