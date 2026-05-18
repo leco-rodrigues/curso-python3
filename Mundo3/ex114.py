a273224a7f6ef609aa260a6fe5824cfe968efc8a
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-18 16:04:58 -0300

    Add challenge 114 solution

--- Mundo3/ex114.py
+++ Mundo3/ex114.py
@@ -2,10 +2,22 @@
     # Crie um código em Python que teste se o site pudim está acessível
     # pelo computador usado.
 
+from utils.strings import exit_message, display_message_box
+import urllib.request
+import urllib.error
+
 # Passo 1: Criar código
+try:
+    requisicao = urllib.request.Request(
+        "https://pudim.com.br",
+        headers={"User-Agent": "Edg/124.0.0.0"}
+    )
+    site = urllib.request.urlopen(requisicao, timeout = 5)
+except urllib.error.URLError:
+    print("O site Pudim não está acessível no momento.")
+else:
+    print("O site Pudim se encontra acessível no momento.")
 
-# Passo 2: Exibir o resultado
-
-# Passo 3: Exibir uma mensagem de encerramento
-
-# -| AULA 23 - TRATAMENTO DE ERROS E EXCEÇÕES | DESAFIO [114]
+# Passo 2: Exibir uma mensagem de encerramento
+display_message_box(exit_message())
+# --------------------------------| AULA 23 - TRATAMENTO DE ERROS E EXCEÇÕES | DESAFIO [114]
