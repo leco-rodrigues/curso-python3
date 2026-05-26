ec67abe45fbb9357a6d310be3f94ea96fe3612c4
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-26 14:30:03 -0300

    refactor: normalize color names in text_color()

--- utils/colors/__init__.py
+++ utils/colors/__init__.py
@@ -1,5 +1,5 @@
 def text_color(text: str, color: str = None) -> str:
     colors: dict[str, str] = {"vermelho": "\033[31m", "verde": "\033[32m",
                               "amarelo": "\033[33m", "azul": "\033[34m"}
-    text = f"{colors[color]}{text}\033[m"
+    text = f"{colors[color.lower()]}{text}\033[m"
     return text
