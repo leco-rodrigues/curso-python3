42f998f53e9338109b290be5ad56fd251526d35b
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-26 14:14:05 -0300

    refactor: make color parameter optional in text_color()

--- utils/colors/__init__.py
+++ utils/colors/__init__.py
@@ -1,4 +1,4 @@
-def text_color(text: str, color: str) -> str:
+def text_color(text: str, color: str = None) -> str:
     colors: dict[str, str] = {"vermelho": "\033[31m", "verde": "\033[32m",
                               "amarelo": "\033[33m", "azul": "\033[34m"}
     text = f"{colors[color]}{text}\033[m"
