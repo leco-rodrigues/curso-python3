67b755bc130a7ad0720bc3e09fceae7d470e5f37
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-27 15:42:49 -0300

    feat: add ex05 solution

--- Mundo4/ex05.py
+++ Mundo4/ex05.py
@@ -1,5 +1,5 @@
 """
-Na sigla OOAD, as dúltimas duas letras significam...
+Na sigla OOAD, as últimas duas letras significam...
 
 A - Algol/Dashbord
 B - Algorithms/Digital
@@ -7,4 +7,14 @@ C - Analytics/Default
 D - Analysis/Design *
 """
 
+from utils.strings import exit_message, display_message_box
+from utils.colors import text_color
+from perguntas import pergunta
+
+# Passo 1: Responder à pergunta
+pergunta("Na sigla OOAD, as últimas duas letras significam...",
+         "Algol/Dashbord", "Algorithms/Digital", "Analytics/Default", "Analysis/Design", "D")
+
+# Passo 2: Exibir uma mensagem de encerramento
+display_message_box(text_color(exit_message(), "amarelo"))
 # -------------------------------------------------------| AULA 01 - CRISE DO SOFTWARE: O MOTIVO DO SURGIMENTO DA POO | DESAFIO 05
