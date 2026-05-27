51321378cb1e86d79a1b50c96e590933a50b9e2c
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-27 15:41:09 -0300

    feat: add ex04 solution

--- Mundo4/ex04.py
+++ Mundo4/ex04.py
@@ -7,4 +7,13 @@ C - SmallTalk
 D - Python
 """
 
+from utils.strings import exit_message, display_message_box
+from utils.colors import text_color
+from perguntas import pergunta
+
+# Passo 1: Responder à pergunta
+pergunta("A linguagem Simula foi um superconjunto de que outra linguagem?", "ALGOL", "C", "SmallTalk", "Python", "A")
+
+# Passo 2: Exibir uma mensagem de encerramento
+display_message_box(text_color(exit_message(), "amarelo"))
 # -------------------------------------------------------| AULA 01 - CRISE DO SOFTWARE: O MOTIVO DO SURGIMENTO DA POO | DESAFIO 04
