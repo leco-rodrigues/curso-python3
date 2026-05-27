9184983322e722a706cb6d6893c987084e3c614f
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-27 15:36:09 -0300

    feat: add ex02 solution

--- Mundo4/ex02.py
+++ Mundo4/ex02.py
@@ -6,5 +6,14 @@ B - Desvios forçados com GOTO *
 C - Estruturas de controle
 D - Instruções de baixo nível
 """
+from utils.strings import exit_message, display_message_box
+from utils.colors import text_color
+from perguntas import pergunta
 
+# Passo 1: Responder à pergunta
+pergunta("Qual das características a seguir identifica as primeiras linguagens lineares?",
+         "Maior modularidade", "Desvios forçados com GOTO", "Estruturas de ocntrole", "Instruções de baixo nível", "B")
+
+# Passo 2: Exibir uma mensagem de encerramento
+display_message_box(text_color(exit_message(), "amarelo"))
 # -------------------------------------------------------| AULA 01 - CRISE DO SOFTWARE: O MOTIVO DO SURGIMENTO DA POO | DESAFIO 02
