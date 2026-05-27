7b876071acd52ecc17a2dba12fb0539015c7855d
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-27 15:34:38 -0300

    feat: add ex01 solution

--- Mundo4/ex01.py
+++ Mundo4/ex01.py
@@ -7,4 +7,13 @@ C - 1980
 D - 2000
 """
 
+from utils.strings import exit_message, display_message_box
+from utils.colors import text_color
+from perguntas import pergunta
+
+# Passo 1: Responder à pergunta
+pergunta("Em que década aconteceu a 'Crise do Software' no mercado?", "1950", "1960", "1980", "2000", "B")
+
+# Passo 2: Exibir uma mensagem de encerramento
+display_message_box(text_color(exit_message(), "amarelo"))
 # -------------------------------------------------------| AULA 01 - CRISE DO SOFTWARE: O MOTIVO DO SURGIMENTO DA POO | DESAFIO 01
