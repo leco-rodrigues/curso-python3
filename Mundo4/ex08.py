2f68a1b5a1190babf49528feb240a17e192a63f0
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-28 14:31:30 -0300

    feat: add challenge ex08 solution

--- Mundo4/ex08.py
+++ Mundo4/ex08.py
@@ -8,9 +8,13 @@ C - Confiável
 D - Manutenível
 """
 
-# Passo 1: Responder à pergunta
+from utils.strings import exit_message, display_message_box
+from utils.colors import text_color
+from perguntas import pergunta
 
+# Passo 1: Responder à pergunta
+pergunta("Posso desenvolver várias funções que dependem uma da outra ao mesmo tempo se o planejamento for seguido.", "Oportuno", "Reutilizável", "Confiável", "Manutenível", "A")
 
 # Passo 2: Exibir uma mensagem de encerramento
-
+display_message_box(text_color(exit_message(), "amarelo"))
 # -------------------------------------------------------| AULA 02 - AS 6 VANTAGENS DA POO: PROGRAMAÇÃO ORIENTADA A OBJETOS | DESAFIO 08
