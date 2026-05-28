4d61ca8893ac790684077b5dc33ca4ddd28750f1
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-28 19:28:13 -0300

    feat: add challenge ex14 solution

--- Mundo4/ex14.py
+++ Mundo4/ex14.py
@@ -1,6 +1,5 @@
 """
-Ao conjunto de todos os valores de atributos de um objeto
-em um determinado momento, damos o nome de:
+Ao conjunto de todos os valores de atributos de um objeto em um determinado momento, damos o nome de:
 
 A - identidade
 B - instância
@@ -8,8 +7,15 @@ C - método
 D - estado *
 """
 
+from utils.strings import exit_message, display_message_box
+from utils.colors import text_color
+from perguntas import pergunta
+
 # Passo 1: Responder à pergunta
 
-# Passo 2: Exibir uma mensagem de encerramento
+pergunta("Ao conjunto de todos os valores de atributos de um objeto em um determinado momento, damos o nome de:",
+         "identidade", "instância", "método", "estado", "D")
 
+# Passo 2: Exibir uma mensagem de encerramento
+display_message_box(text_color(exit_message(), "amarelo"))
 # -------------------------------------------------------| AULA 03 - POO PARA INICIANTES: GUIA DEFINITIVO - CLASSES, OBJETOS, ATRIBUTOS, ETC | DESAFIO 14
