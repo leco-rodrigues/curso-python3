0430095b8d07627c1a31b0db24a4591d3e5ef4f0
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-28 19:22:13 -0300

    feat: add challenge ex11 solution

--- Mundo4/ex11.py
+++ Mundo4/ex11.py
@@ -7,8 +7,13 @@ C - Notificação *
 D - Livro
 """
 
+from utils.strings import exit_message, display_message_box
+from utils.colors import text_color
+from perguntas import pergunta
+
 # Passo 1: Responder à pergunta
+pergunta("Qual dos objetos abaixo seria o único abstrato da lista?", "Sensor", "Robô", "Notificação", "Livro", "C")
 
 # Passo 2: Exibir uma mensagem de encerramento
-
+display_message_box(text_color(exit_message(), "amarelo"))
 # -------------------------------------------------------| AULA 03 - POO PARA INICIANTES: GUIA DEFINITIVO - CLASSES, OBJETOS, ATRIBUTOS, ETC | DESAFIO 11
