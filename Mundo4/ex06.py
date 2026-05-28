6759189c824516457d689de0999599d5f1543246
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-28 14:27:30 -0300

    feat: add challenge ex06 solution

--- Mundo4/ex06.py
+++ Mundo4/ex06.py
@@ -8,9 +8,14 @@ C - Manutenível
 D - Reutilizável *
 """
 
-# Passo 1: Responder à pergunta
+from utils.strings import exit_message, display_message_box
+from utils.colors import text_color
+from perguntas import pergunta
 
+# Passo 1: Responder à pergunta
+pergunta("Criei um sistema para Escola, ao criar outro sistema para Academia, parte do trabalho já estará feito, pois tenho Alunos nos dois.", 
+         "Confiável", "Natural", "Manutenível", "Reutilizável", "D")
 
 # Passo 2: Exibir uma mensagem de encerramento
-
+display_message_box(text_color(exit_message(), "amarelo"))
 # -------------------------------------------------------| AULA 02 - AS 6 VANTAGENS DA POO: PROGRAMAÇÃO ORIENTADA A OBJETOS | DESAFIO 06
