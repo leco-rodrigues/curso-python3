b3cb80c1d11ea721fad41873801828365e2083b2
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-28 14:34:39 -0300

    feat: add challenge ex10 solution

--- Mundo4/ex10.py
+++ Mundo4/ex10.py
@@ -8,9 +8,13 @@ C - Confiável *
 D - Natural
 """
 
-# Passo 1: Responder à pergunta
+from utils.strings import exit_message, display_message_box
+from utils.colors import text_color
+from perguntas import pergunta
 
+# Passo 1: Responder à pergunta
+pergunta("Eu posso alterar como uma funcionalidade trabalha e tudo continua funcionando como antes.", "Oportuno", "Reutilizável", "Confiável", "Natural", "C")
 
 # Passo 2: Exibir uma mensagem de encerramento
-
+display_message_box(text_color(exit_message(), "amarelo"))
 # -------------------------------------------------------| AULA 02 - AS 6 VANTAGENS DA POO: PROGRAMAÇÃO ORIENTADA A OBJETOS | DESAFIO 10
