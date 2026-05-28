6dd50f2a5e5cce0ba7edb53981ac6f6e3ac4b4eb
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-28 19:26:13 -0300

    feat: add challenge ex13 solution

--- Mundo4/ex13.py
+++ Mundo4/ex13.py
@@ -1,6 +1,5 @@
 """
-Todo objeto tem uma lista de atividades que podem ser feitas com ele ou
-que ele pode desempenhar sozinho, que são:
+Todo objeto tem uma lista de atividades que podem ser feitas com ele ou que ele pode desempenhar sozinho, que são:
 
 A - atributos
 B - métodos *
@@ -8,8 +7,14 @@ C - classes
 D - instâncias
 """
 
+from utils.strings import exit_message, display_message_box
+from utils.colors import text_color
+from perguntas import pergunta
+
 # Passo 1: Responder à pergunta
+pergunta("Todo objeto tem uma lista de atividades que podem se feitas com ele ou que ele pode desmepenhar sozinho, que são:",
+         "atributos", "métodos", "classes", "instâncias", "B")
 
 # Passo 2: Exibir uma mensagem de encerramento
-
+display_message_box(text_color(exit_message(), "amarelo"))
 # -------------------------------------------------------| AULA 03 - POO PARA INICIANTES: GUIA DEFINITIVO - CLASSES, OBJETOS, ATRIBUTOS, ETC | DESAFIO 13
