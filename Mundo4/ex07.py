d73f2495c649d88a38d64ed030078b47bbd0355e
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-05-28 14:29:36 -0300

    feat: add challenge ex07 solution

--- Mundo4/ex07.py
+++ Mundo4/ex07.py
@@ -8,9 +8,13 @@ C - Reutilizável
 D - Oportuno
 """
 
-# Passo 1: Responder à pergunta
+from utils.strings import exit_message, display_message_box
+from utils.colors import text_color
+from perguntas import pergunta
 
+# Passo 1: Responder à pergunta
+pergunta("Se precisar adicionar uma nova funcionalidade ao sistema, consigo fazer isso com grande facilidade.", "Natural", "Extensível", "Reutilizável", "Oportuno", "B")
 
 # Passo 2: Exibir uma mensagem de encerramento
-
+display_message_box(text_color(exit_message(), "amarelo"))
 # -------------------------------------------------------| AULA 02 - AS 6 VANTAGENS DA POO: PROGRAMAÇÃO ORIENTADA A OBJETOS | DESAFIO 07
