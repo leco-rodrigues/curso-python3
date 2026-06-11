704e8ae21fa7a7fe70bbd1cf6235a927d4f5b974
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-06-11 20:52:46 -0300

    feat: add challenge ex17 solution

--- Mundo4/ex17.py
+++ Mundo4/ex17.py
@@ -1,8 +1,26 @@
 # Crie a classe Produto, onde podemos cadastrar nome e o preço.
 # Crie também um método que mostre uma etiqueta de preço do produto.
 
+from rich import print
+from rich.panel import Panel
+
 # Passo 1: Criar classe Produto
+class Produto:
+    def __init__(self, nome: str, preco: float):
+        self.nome = nome
+        self.preco = preco
 
-# Passo 2: Criar um método para mostrar a etiqueta de preço
+# Passo 2: Criar um método para mostrar a etiqueta
+    def etiqueta(self):
+        caixa = Panel(f"{self.nome}\nR${self.preco:,.2f}", title="Produto", width=30, padding=(0,6))
+        return caixa
+        
+# Passo 3: Exibir o resultado
 
-# -| AULA 06 - Aprenda Programação Orientada a Objetos com Desafios em Python | DESAFIO 17
+p1 = Produto("Cadeira Gamer", 1_000.00)
+print(p1.etiqueta())
+
+p2 = Produto("Motorola G9 Play", 1_200.00)
+print(p2.etiqueta())
+
+# -| AULA 06 - APRENDA PROGRAMAÇÃO ORIENTADA A OBJETOS COM DESAFIOS EM PYTHON | DESAFIO 17
