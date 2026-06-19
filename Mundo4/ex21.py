c9c0208ad8002cf19e4215dbc31cd5b171f22134
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-06-19 19:18:18 -0300

    feat: add challenge ex21 solution

--- Mundo4/ex21.py
+++ Mundo4/ex21.py
@@ -1,8 +1,42 @@
 # Crie a classe Caneta, que simule o funcionamento de uma caneta colorida,
 # podendo escrever frases na cor relativa.
 
+from rich import print
+
 # Passo 1: Criar classe Caneta
+class Caneta:
+    tampada: bool = True
 
-# Passo 2: Criar método para escrever
+    def __init__(self, cor: str):
+        self.cor = cor
 
-# -| AULA 06 - Aprenda Programação Orientada a Objetos com Desafios em Python | DESAFIO 21
+# Passo 2: Criar método para destampar
+    def destampar(self):
+        self.tampada = False
+
+# Passo 3: Criar método para escrever
+    def escrever(self, texto:str):
+        cores: dict[str, str] = {"azul":"blue", "vermelho":"red", "verde":"green"}
+
+        print(f"[{cores[self.cor]}]{texto}[/]", end = "")
+
+# Passo 4: Criar método para quebrar linha
+    def quebrar_linha(self, numero: int):
+        for i in range(1, numero):
+            print("\n")
+
+# Passo 5: Exibir o resultado
+c1 = Caneta("azul")
+c2 = Caneta("vermelho")
+c3 = Caneta("verde")
+
+c1.destampar()
+c2.destampar()
+c3.destampar()
+
+c1.escrever("Olá, tudo bem? ")
+c1.quebrar_linha(2)
+c2.escrever("Olá, Gafanhoto! ")
+c3.escrever("Vamos exercitar!")
+
+# -| AULA 06 - APRENDA PROGRAMAÇÃO ORIENTADA A OBJETOS COM DESAFIOS EM PYTHON | DESAFIO 21
