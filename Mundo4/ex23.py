5c060a702aefc250c31c383bb31a565fa1180d56
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-08-04 14:05:58 -0300

    refactor: apply various code improvements

--- Mundo4/ex23.py
+++ Mundo4/ex23.py
@@ -5,55 +5,60 @@
         # Circulo(Poligono) + raio + perimetro() + area()
 
 from abc import ABC, abstractmethod
+from math import pi
+
 
 # Passo 1: Criar classe abstrata Poligono
 class Poligono(ABC):
+
     def __init__(self, qtd_lados: int) -> None:
         self.qtd_lados = qtd_lados
 
 # Passo 2: Criar método abstrato perimetro()
     @abstractmethod
-    def perimetro(self) -> None:
+    def perimetro(self) -> float:
         pass
 
 # Passo 3: Criar método abstrato area()
     @abstractmethod
-    def area(self) -> None:
+    def area(self) -> float:
         pass
 
 # Passo 4: Criar subclasse Quadrado
 class Quadrado(Poligono):
-    def __init__(self, qtd_lados: int, lado: float) -> None:
-        super().__init__(qtd_lados)
+
+    def __init__(self, lado: int = 1) -> None:
+        super().__init__(4)
         self.lado = lado
 
-    def perimetro(self) -> None:
-        valor_perimetro: float = 4 * self.lado
-        print(f"O perímetro do quadrado é igual a {valor_perimetro}.")
+    def perimetro(self) -> float:
+        return self.lado * 4
 
-    def area(self) -> None:
-        valor_area: float = self.lado * self.lado
-        print(f"A área do quadrado é igual a {valor_area}.")
+    def area(self) -> float:
+        return self.lado ** 2
 
 # Passo 5: Criar subclasse Circulo
 class Circulo(Poligono):
-    def __init__(self, qtd_lados: int, raio: float) -> None:
-        super().__init__(qtd_lados)
+
+    def __init__(self, raio: float = 1) -> None:
+        super().__init__(0)
         self.raio = raio
 
-    def perimetro(self) -> None:
-        PI: float = 3.14
-        valor_perimetro: float = 2 * PI * self.raio
-        print(f"O perímetro do círculo é igual a {valor_perimetro:.1f}.")
+    def perimetro(self) -> float:
+        return 2 * pi * self.raio
 
-    def area(self) -> None:
-        PI: float = 3.14
-        valor_area: float = PI * (self.raio * self.raio)
-        print(f"A área do círculo é igual a {valor_area:.1f}.")
+    def area(self) -> float:
+        return pi * self.raio ** 2
 
 # Passo 6: Exibir o resultado
-q1: Circulo = Circulo(4, 20)
-q1.perimetro()
-q1.area()
+q: Quadrado = Quadrado(20)
+
+print(f"Perímetro = {q.perimetro():.1f}mm")
+print(f"Área = {q.area():.1f}mm²")
+
+c: Circulo = Circulo(12)
+
+print(f"Perímetro = {c.perimetro():.1f}cm")
+print(f"Área = {c.area():.1f}cm²")
 
 # -| AULA 09 - DESAFIOS DE HERANÇA, ABSTRAÇÃO E CLASSES | DESAFIO 23
